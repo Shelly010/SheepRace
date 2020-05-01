99678c8bcd8a88248ce4064e63386cfc0130f7e4
Satpreet Singh <singhsatpreet29@gmail.com>
2020-05-01 21:56:41 +1200

    Add project files.

---
+++ SheepRace/Factory.cs
@@ -0,0 +1,18 @@
+﻿namespace SheepRace
+{
+    public static class Factory
+    {
+        // Decides which class to instantiate
+        public static Punter GetAPunter(int id)
+        {
+            switch (id)
+            {
+                case 0: return new Satpreet();
+                case 1: return new Dilpreet();
+                case 2: return new Shelly();
+                default: return null;
+            }
+        }
+
+    }
+}
